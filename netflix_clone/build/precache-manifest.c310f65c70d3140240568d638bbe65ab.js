0442b225905d64dafb3b1485110b5717f4e14384
Zoltan Karpati <37480888+itkarpathy@users.noreply.github.com>
2020-09-28 16:30:17 +0200

    all files added
    full website files

---
+++ netflix_clone/build/precache-manifest.c310f65c70d3140240568d638bbe65ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5cd7dec02ad69fa560915d07e83e39d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b711df6b270c34a9f4f",
+    "url": "/static/css/main.7fabf2c1.chunk.css"
+  },
+  {
+    "revision": "08b6306a8926ee98d90d",
+    "url": "/static/js/2.c134feaf.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.c134feaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b711df6b270c34a9f4f",
+    "url": "/static/js/main.f13ab0f2.chunk.js"
+  },
+  {
+    "revision": "439ae095c7fe9cd37d99",
+    "url": "/static/js/runtime-main.0ea695f4.js"
+  }
+]);
